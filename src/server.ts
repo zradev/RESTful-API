74b7b016ac89d4bbd5373411940bd09ee8a41fad
zradev <zlatkoradevv@gmail.com>
2023-01-28 16:06:22 +0200

    Add Custom Logging Module

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import mongoose from "mongoose";
 import { config } from "./config/config";
+import Logging from "./library/Logging";
 
 const router = express();
 
@@ -10,8 +11,9 @@ mongoose.set("strictQuery", false);
 mongoose
     .connect(config.mongo.url)
     .then(() => {
-        console.log("Successfully Conntected to MongoDB");
+        Logging.info("Successfully Conntected to MongoDB");
     })
     .catch((error) => {
-        console.log(error);
+        Logging.error("Unable to conntect to MongoDB");
+        Logging.error(error);
     });
