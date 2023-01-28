cc365c8c9b00d49ffff602170432a0ce174c2c36
zradev <zlatkoradevv@gmail.com>
2023-01-28 16:26:15 +0200

    Add StartServer function in server.ts with API Rules and Error Handling

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,55 @@ mongoose
     .connect(config.mongo.url)
     .then(() => {
         Logging.info("Successfully Conntected to MongoDB");
+        StartServer();
     })
     .catch((error) => {
         Logging.error("Unable to conntect to MongoDB");
         Logging.error(error);
     });
+
+// Strat the server if Mongo connects
+const StartServer = () => {
+    router.use((req, res, next) => {
+        // Log the Request
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on("finish", () => {
+            // Log the Response
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    // API Rules
+    router.use((req, res, next) => {
+        // Replace * with a list of IPs if u want this to be private
+        res.header("Access-Control-Allow-Origin", "*");
+        // Allowed Headers
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+        if (req.method == "OPTIONS") {
+            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+    // Routes
+
+    // Healthcheck
+    router.get("/ping", (req, res, next) => res.status(200).json({ message: "pong" }));
+
+    // Error Handling
+    router.use((req, res, next) => {
+        const error = new Error("Not Found");
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running of port ${config.server.port}.`));
+};
